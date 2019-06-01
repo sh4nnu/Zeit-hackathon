e1b0f25e846cab97097fb5235ca9c842e6e604ee
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2019-06-01 19:23:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const {withUiHook, htm} = require('@zeit/integration-utils');
 
 const store = {
-	accountSID: '',
-	authKey: ''
+  accountSID: '',
+  authKey: ''
 };
 
 module.exports = withUiHook(async ({payload}) => {
-	const {clientState, action} = payload;
+  const {clientState, action} = payload;
 
-	if (action === 'submit') {
-		store.accountSID = clientState.accountSID;
-		store.authKey = clientState.authKey;
-	}
+  if (action === 'submit') {
+    store.accountSID = clientState.accountSID;
+    store.authKey = clientState.authKey;
+  }
 
-	if (action === 'reset') {
-		store.accountSID = '';
-		store.authKey = '';
-	}
+  if (action === 'reset') {
+    store.accountSID = '';
+    store.authKey = '';
+  }
 
-	return htm
-		<Page>
-			<Container>
-				<Input label="SID" name="accountSID" value=${store.accountSID} />
-				<Input label="Auth Key" name="authKey" value=${store.authKey} />
-			</Container>
-			<Container>
-				<Button action="submit">Submit</Button>
-				<Button action="reset">Reset</Button>
-			</Container>
-			<AutoRefresh timeout=${3000} />
-		</Page>
-	`
+  return htm`
+    <Page>
+      <Container>
+        <Input label="SID" name="accountSID" value=${store.accountSID} />
+        <Input label="Auth Key" name="authKey" value=${store.authKey} />
+      </Container>
+      <Container>
+        <Button action="submit">Submit</Button>
+        <Button action="reset">Reset</Button>
+      </Container>
+      <AutoRefresh timeout=${3000} />
+    </Page>
+  `
 });
